34f7007bc639644698bcba32df85a18aa780cbb5
Luís M T Silva <44646310+LuisMMMTS@users.noreply.github.com>
2021-01-31 18:32:53 +0000

    added auxilar file, changed main.py to include my auxiliar file

---
+++ main.py
@@ -0,0 +1,7 @@
+include myAuxiliarFile
+
+print("hello world")
+
+
+def main():
+    print("well this now has a main and an auxiliar file with functions, that what i did in branch feature 1 did")
---
+++ myAuxilarFile.py
@@ -0,0 +1,4 @@
+def auxiliarFunction():
+    print("yes, this function serves no purpose")
+    print("Este ficheiro não vai gerar conflitos pois ainda não existe no master")
+    return
